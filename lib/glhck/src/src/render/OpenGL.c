565bfdff98220b98cfec8a71aa446a294b30c1d5
Cloudef <mailRoxas@gmail.com>
2012-04-15 15:07:10 +0300

    Wrap OpenGL functions to GL_CALL macro

--- lib/glhck/src/src/render/OpenGL.c
+++ lib/glhck/src/src/render/OpenGL.c
@@ -16,7 +16,7 @@ typedef struct __OpenGLrender {
 } __OpenGLrender;
 static __OpenGLrender _OpenGL;
 
-/* helper macros */
+/* declare gl generation function */
 #define DECLARE_GL_GEN_FUNC(x,y)                                     \
 static void x(unsigned int count, unsigned int *objects)             \
 {                                                                    \
@@ -24,6 +24,7 @@ static void x(unsigned int count, unsigned int *objects)             \
    y(count, objects);                                                \
 }
 
+/* declare gl bind function */
 #define DECLARE_GL_BIND_FUNC(x,y)                                    \
 static void x(unsigned int object)                                   \
 {                                                                    \
@@ -31,90 +32,37 @@ static void x(unsigned int object)                                   \
    y;                                                                \
 }
 
-#define GL_ERROR(x)                                                  \
-{                                                                    \
-GLenum error;                                                        \
-if (GL_DEBUG) {                                                      \
-   if ((error = glGetError())                                        \
-         != GL_NO_ERROR)                                             \
-      DEBUG(GLHCK_DBG_ERROR, "GL @%-20s %-20s >> %s",                \
-            __func__, x,                                             \
-            error==GL_INVALID_ENUM?                                  \
-            "GL_INVALID_ENUM":                                       \
-            error==GL_INVALID_VALUE?                                 \
-            "GL_INVALID_VALUE":                                      \
-            error==GL_INVALID_OPERATION?                             \
-            "GL_INVALID_OPERATION":                                  \
-            error==GL_STACK_OVERFLOW?                                \
-            "GL_STACK_OVERFLOW":                                     \
-            error==GL_STACK_UNDERFLOW?                               \
-            "GL_STACK_UNDERFLOW":                                    \
-            error==GL_OUT_OF_MEMORY?                                 \
-            "GL_OUT_OF_MEMORY":                                      \
-            error==GL_TABLE_TOO_LARGE?                               \
-            "GL_TABLE_TOO_LARGE":                                    \
-            error==GL_INVALID_OPERATION?                             \
-            "GL_INVALID_OPERATION":                                  \
-            "GL_UNKNOWN_ERROR");                                     \
-}                                                                    \
-}
-
-static void geometryPointer(__GLHCKobjectGeometry *geometry)
+/* check gl errors on debug build */
+#ifdef NDEBUG
+#  define GL_CALL(x) x
+#else
+#  define GL_CALL(x) x; GL_ERROR(__func__, __STRING(x));
+static inline void GL_ERROR(const char *func, const char *glfunc)
 {
-   /* vertex data */
-   if (geometry->vertexData) {
-      glVertexPointer(3, GLHCK_PRECISION_VERTEX,
-            sizeof(glhckVertexData), &geometry->vertexData[0].vertex);
-      glNormalPointer(GLHCK_PRECISION_VERTEX,
-            sizeof(glhckVertexData), &geometry->vertexData[0].normal);
-#if GLHCK_VERTEXDATA_COLOR
-      glColorPointer(4, GLHCK_PRECISION_COLOR,
-            sizeof(glhckVertexData), &geometry->vertexData[0].color);
+   GLenum error;
+   if ((error = glGetError())
+         != GL_NO_ERROR)
+      DEBUG(GLHCK_DBG_ERROR, "GL @%-20s %-20s >> %s",
+            func, glfunc,
+            error==GL_INVALID_ENUM?
+            "GL_INVALID_ENUM":
+            error==GL_INVALID_VALUE?
+            "GL_INVALID_VALUE":
+            error==GL_INVALID_OPERATION?
+            "GL_INVALID_OPERATION":
+            error==GL_STACK_OVERFLOW?
+            "GL_STACK_OVERFLOW":
+            error==GL_STACK_UNDERFLOW?
+            "GL_STACK_UNDERFLOW":
+            error==GL_OUT_OF_MEMORY?
+            "GL_OUT_OF_MEMORY":
+            error==GL_TABLE_TOO_LARGE?
+            "GL_TABLE_TOO_LARGE":
+            error==GL_INVALID_OPERATION?
+            "GL_INVALID_OPERATION":
+            "GL_UNKNOWN_ERROR");
+}
 #endif
-      GL_ERROR("glVertexPointer()");
-   }
-
-   /* texture data */
-   if (geometry->textureData) {
-      glTexCoordPointer(2, GLHCK_PRECISION_COORD,
-            sizeof(glhckTextureData), &geometry->textureData[0].coord);
-      GL_ERROR("glVertexPointer()");
-   }
-}
-
-static int renderInfo(void)
-{
-   char *version, *vendor, *extensions;
-   unsigned int major, minor, patch;
-   int num_parsed;
-   TRACE();
-
-   version = (char*)glGetString(GL_VERSION);
-   vendor  = (char*)glGetString(GL_VENDOR);
-
-   if (!version || !vendor)
-      goto fail;
-
-   num_parsed = sscanf(version, "%u.%u.%u",
-         &major, &minor, &patch);
-
-   if (num_parsed == 1) {
-      minor = 0;
-      patch = 0;
-   } else if (num_parsed == 2)
-      patch = 0;
-
-   DEBUG(3, "%s [%u.%u.%u] [%s]\n", RENDER_NAME, major, minor, patch, vendor);
-   if ((extensions = (char*)glGetString(GL_EXTENSIONS)))
-      DEBUG(3, extensions);
-
-   RET("%d", RETURN_OK);
-   return RETURN_OK;
-
-fail:
-   RET("%d", RETURN_FAIL);
-   return RETURN_FAIL;
-}
 
 static int uploadTexture(_glhckTexture *texture, unsigned int flags)
 {
@@ -158,38 +106,104 @@ static void render(void)
    _OpenGL.indicesCount  = 0;
 }
 
+/* \brief pass interleaved vertex data to OpenGL nicely. */
+static inline void geometryPointer(__GLHCKobjectGeometry *geometry)
+{
+   /* vertex data */
+   if (geometry->vertexData) {
+      GL_CALL(glVertexPointer(3, GLHCK_PRECISION_VERTEX,
+               sizeof(glhckVertexData), &geometry->vertexData[0].vertex));
+      GL_CALL(glNormalPointer(GLHCK_PRECISION_VERTEX,
+               sizeof(glhckVertexData), &geometry->vertexData[0].normal));
+      GL_CALL(glTexCoordPointer(2, GLHCK_PRECISION_COORD,
+            sizeof(glhckVertexData), &geometry->vertexData[0].coord));
+#if GLHCK_VERTEXDATA_COLOR
+      GL_CALL(glColorPointer(4, GLHCK_PRECISION_COLOR,
+               sizeof(glhckVertexData), &geometry->vertexData[0].color));
+#endif
+   }
+}
+
+/* \brief draw single object
+ * TODO: In future just add to stack,
+ * and handle actual drawing on glRender()?
+ *
+ * We also need sorting and other crap... */
 static void objectDraw(_glhckObject *object)
 {
    kmMat4 projection;
    unsigned int i;
    CALL("%p", object);
 
-   glMatrixMode(GL_PROJECTION);
-   kmMat4PerspectiveProjection(&projection, 35, 1, 0.1f, 300);
-   glLoadMatrixf((float*)&projection);
-   glMatrixMode(GL_MODELVIEW);
-   glLoadIdentity();
-   glTranslatef(0,0,-10.0f);
+   GL_CALL(glMatrixMode(GL_PROJECTION));
+   kmMat4PerspectiveProjection(&projection, 35,
+         (float)_GLHCKlibrary.render.width/
+         (float)_GLHCKlibrary.render.height, 0.1f, 300);
+   GL_CALL(glLoadMatrixf((float*)&projection));
+   GL_CALL(glMatrixMode(GL_MODELVIEW));
+   GL_CALL(glLoadIdentity());
+   GL_CALL(glTranslatef(0,0,-10.0f));
+   GL_CALL(glRotatef(25,45,15,1));
 
-   glEnableClientState(GL_VERTEX_ARRAY);
-   GL_ERROR("glEnableClientState()");
-
-   glColor3f(1,1,1);
-   GL_ERROR("glColor3f()");
+   GL_CALL(glEnable(GL_TEXTURE_2D));
+   GL_CALL(glEnableClientState(GL_VERTEX_ARRAY));
+   GL_CALL(glEnableClientState(GL_TEXTURE_COORD_ARRAY));
 
    geometryPointer(&object->geometry);
+   GL_CALL(glDrawElements(GL_TRIANGLE_STRIP, object->geometry.indicesCount,
+            GLHCK_PRECISION_INDEX, &object->geometry.indices[0]));
 
-   glDrawElements(GL_TRIANGLE_STRIP, object->geometry.indicesCount,
-         GLHCK_PRECISION_INDEX,
-         &object->geometry.indices[0]);
-   GL_ERROR("glDrawElements()");
+   GL_CALL(glDisableClientState(GL_TEXTURE_COORD_ARRAY));
+   GL_CALL(glDisableClientState(GL_VERTEX_ARRAY));
 
-   glDisableClientState(GL_VERTEX_ARRAY);
-   GL_ERROR("glDisableClientState()");
-
-   _OpenGL.indicesCount  += object->geometry.indicesCount;
+   _OpenGL.indicesCount += object->geometry.indicesCount;
 }
 
+/* \brief get render information */
+static int renderInfo(void)
+{
+   char *version, *vendor, *extensions;
+   unsigned int major, minor, patch;
+   int num_parsed;
+   TRACE();
+
+   version = (char*)GL_CALL(glGetString(GL_VERSION));
+   vendor  = (char*)GL_CALL(glGetString(GL_VENDOR));
+
+   if (!version || !vendor)
+      goto fail;
+
+   num_parsed = sscanf(version, "%u.%u.%u",
+         &major, &minor, &patch);
+
+   if (num_parsed == 1) {
+      minor = 0;
+      patch = 0;
+   } else if (num_parsed == 2)
+      patch = 0;
+
+   DEBUG(3, "%s [%u.%u.%u] [%s]\n", RENDER_NAME, major, minor, patch, vendor);
+   extensions = (char*)GL_CALL(glGetString(GL_EXTENSIONS));
+
+   if (extensions)
+      DEBUG(3, extensions);
+
+   RET("%d", RETURN_OK);
+   return RETURN_OK;
+
+fail:
+   RET("%d", RETURN_FAIL);
+   return RETURN_FAIL;
+}
+
+/* \brief resize viewport */
+static void resize(int width, int height)
+{
+   CALL("%d, %d", width, height);
+   GL_CALL(glViewport(0, 0, width, height));
+}
+
+/* \brief terminate renderer */
 static void terminate(void)
 {
    TRACE();
@@ -210,7 +224,8 @@ void _glhckRenderOpenGL(void)
    TRACE();
 
    /* init OpenGL context */
-   glClear(GL_COLOR_BUFFER_BIT);
+   GL_CALL(glClear(GL_COLOR_BUFFER_BIT));
+   resize(_GLHCKlibrary.render.width, _GLHCKlibrary.render.height);
 
    /* we use GLEW */
    if (glewInit() != GLEW_OK)
@@ -236,7 +251,8 @@ void _glhckRenderOpenGL(void)
    GLHCK_RENDER_FUNC(render, render);
    GLHCK_RENDER_FUNC(objectDraw, objectDraw);
 
-   /* terminate function */
+   /* common */
+   GLHCK_RENDER_FUNC(resize, resize);
    GLHCK_RENDER_FUNC(terminate, terminate);
 
    /* this also tells library that everything went OK,
