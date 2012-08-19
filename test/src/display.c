b130e6da10e78e7f5b7e6bd770185474dbf8c252
Jari Vetoniemi <mailroxas@gmail.com>
2012-08-19 22:25:19 +0300

    Don't skip anything, use the first found

--- test/src/display.c
+++ test/src/display.c
@@ -116,7 +116,7 @@ int main(int argc, char **argv)
    glhckObjectScalef(sprite2, 0.03f, 0.03f, 0.03f);
    glhckObjectPositionf(sprite3, 64*2, 48*2, 0);
 
-#define SKIP_MMD  1
+#define SKIP_MMD  0
 #define SKIP_OCTM 0
 
 #if SKIP_MMD
