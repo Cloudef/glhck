2531ac52010b572374a32a80988906799f85761b
Jari Vetoniemi <mailroxas@gmail.com>
2012-12-07 21:12:24 +0200

    Update frustrum on 2d projection mode

--- example/src/display.c
+++ example/src/display.c
@@ -234,6 +234,7 @@ int main(int argc, char **argv)
          glhckRenderSetProjection(&identity);
       } else if (glfwGetKey(window, GLFW_KEY_I)) {
          kmMat4 mat2d, pos;
+         glhckCameraUpdate(camera); /* updates the frustum */
          kmMat4Translation(&pos, -cameraPos.x, -cameraPos.y, -cameraPos.z);
          kmMat4Scaling(&mat2d, -2.0f/WIDTH, 2.0f/HEIGHT, 0.0f);
          kmMat4Multiply(&mat2d, &mat2d, &pos);
