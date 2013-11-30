a7528aacba07d247a354112c42224e0da04d0217
Jari Vetoniemi <mailroxas@gmail.com>
2013-11-30 22:12:44 +0200

    This header surely isn't used anywhere?

--- src/render/helper_opengl.c
+++ src/render/helper_opengl.c
@@ -5,10 +5,6 @@
 #define GLHCK_CHANNEL GLHCK_CHANNEL_RENDER
 #include "helper_opengl.h"
 
-#if GLHCK_USE_GLES1 || GLHCK_USE_GLES2
-#  include <EGL/egl.h>
-#endif
-
 #ifndef GLEW_OES_framebuffer_object
 #  define GLEW_OES_framebuffer_object 0
 #endif
